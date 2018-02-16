071607c8d5d9106a2d350789b3cd22231263b4ba
ryanzola <ryanzola@me.com>
2018-02-16 16:52:43 -0500

    work log created. deleted some boiler plate react. trying to figure out of installing things from npm and yarn breaks stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import Main from './main/main';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+
   render() {
     return (
       <div className="App">
-
+        <Main />
       </div>
     );
   }
